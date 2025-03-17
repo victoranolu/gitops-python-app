524187a6a8cf4a935cae33bf192e08cfcb7359aa
victoranolu <afactor05@gmail.com>
2025-03-17 10:17:56 +0100

    edited app.py file

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def health():
 
 @app.route("/")
 def home():
-    return "Victor the DevOps Guy"
+    return "Victor the DevOps Guy trying out the webhook"
    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
